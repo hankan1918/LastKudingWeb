b26bcb9625f8b4a1e1f3b2bc772e2ecd4b247eb0
hankan1918 <65445028+hankan1918@users.noreply.github.com>
2023-04-10 19:22:33 +0900

    Update focusTimer.js

--- js/focusTimer.js
+++ js/focusTimer.js
@@ -6,21 +6,48 @@ const achievement = document.querySelector("#achievement");
 const focusTimer = document.querySelector('#focusTimer');
 const FOCUSACHIKEY = 'achi';
 const FOCUSTIMERKEY = 'times';
+const FOCUSENDKEY ='endtime';
 const local = window.localStorage;
 const date = new Date();
+
+if(local.getItem(FOCUSTIMERKEY)!=undefined&&local.getItem(FOCUSACHIKEY)!=undefined){
+    drawTimer();
+    hideInputForm();
+}
+
+function hideInputForm(){
+    focusInput.classList.add('hidden');
+    focusTimer.classList.remove('hidden');
+}
+
+function showInputForm(){
+    focusTimer.classList.add('hidden');
+    focusInput.classList.remove('hidden');
+}
+
+function drawTimer(){
+    achievement.innerText = local.getItem(FOCUSACHIKEY);
+    timer.innerText = local.getItem(FOCUSLEFTKEY);
+}
 
 function setStorageHandler(event){
     event.preventDefault();
     console.log(achievementInput.value, timeInput.value);
     local.setItem(FOCUSTIMERKEY,timeInput.value);
+    const hours = String(date.getHours()).padStart(2,0);
+    const minutes = String(date.getMinutes()).padStart(2,0);
+    const seconds = String(date.getSeconds()).padStart(2,0);
+    const settime = `${hours}:${minutes}:${seconds}`;
+    local.setItem(FOCUSENDKEY, settime)
     local.setItem(FOCUSACHIKEY, achievementInput.value);
-    focusInput.classList.add('hidden');
-    focusTimer.classList.remove('hidden');
-}
+    drawTimer();
+    hideInputForm();
 
-function drawTimer(){
-    achievement.innerText = local.getItem(FOCUSTIMERKEY);
-    timer.innerText = local.getItem(focusTimer)
+
+    // setInterval(() => {
+    //     local.setItem(FOCUSLEFTKEY)=date.getMinutes()-Number(local.getItem(FOCUSTIMERKEY));
+    //     drawTimer();
+    // }, 1000);
 }
 
 focusInput.addEventListener("submit", setStorageHandler);
