a4b1e6c6e61887deff2b820771edd4e6eb7078ca
hankan1918 <65445028+hankan1918@users.noreply.github.com>
2023-04-10 12:56:47 +0900

    timer

--- js/focusTimer.js
+++ js/focusTimer.js
@@ -1,45 +1,26 @@
 const timeInput = document.querySelector("#timeInput");
 const achievementInput = document.querySelector("#achievementInput");
+const focusInput = document.querySelector("#focusInput");
 const timer = document.querySelector("#timer");
 const achievement = document.querySelector("#achievement");
-const focusInput = document.querySelector("#focusInput");
+const focusTimer = document.querySelector('#focusTimer');
+const FOCUSACHIKEY = 'achi';
+const FOCUSTIMERKEY = 'times';
+const local = window.localStorage;
 const date = new Date();
-let leftTime;
-const STORAGEKEY = 'setTime'
-const setTime = window.localStorage.getItem(STORAGEKEY);
 
-if(setTime == undefined){
-    waitFocusMode();
-}
-
-function waitFocusMode(){
-    focusInput.classList.remove("hidden");
-    achievement.classList.add("hidden");
-    timer.classList.add("hidden");
-}
-
-function OnFocusMode(event){
+function setStorageHandler(event){
     event.preventDefault();
-    console.log(timeInput.value, achievementInput.value);
-    achievement.innerText = achievementInput.value;
-    window.localStorage.setItem(STORAGEKEY,timeInput.value);
-    drawLeftTime();
-    focusInput.classList.add("hidden");
-    achievement.classList.remove("hidden");
-    timer.classList.remove("hidden");
-
-    if(leftTime==0){
-        alert(`Time for Focus on ${achievementInput.value} is over!`);
-        waitFocusMode();
-    }
+    console.log(achievementInput.value, timeInput.value);
+    local.setItem(FOCUSTIMERKEY,timeInput.value);
+    local.setItem(FOCUSACHIKEY, achievementInput.value);
+    focusInput.classList.add('hidden');
+    focusTimer.classList.remove('hidden');
 }
-function drawLeftTime(){
-    // setTime = window.localStorage.getItem(STORAGEKEY);
-    timer.innerText = `${abs(date.getMinutes() - Number(setTime))}M`;
-}
-focusInput.addEventListener("submit", OnFocusMode);
 
-setInterval(() => {
-    
-    drawLeftTime();
-}, 1000);
+function drawTimer(){
+    achievement.innerText = local.getItem(FOCUSTIMERKEY);
+    timer.innerText = local.getItem(focusTimer)
+}
+
+focusInput.addEventListener("submit", setStorageHandler);
