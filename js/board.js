de9997946688d626c4f59cb554f73249ae5f1b37
hankan1918 <angertiger0941@gmail.com>
2023-04-22 18:28:21 +0900

    board

--- js/board.js
+++ js/board.js
@@ -1,23 +1,64 @@
-const inputField = document.querySelector("#inputField");
-const inputButton = document.querySelector("#inputButton");
-const board = document.querySelector("#board");
+// 게시글을 저장할 배열
+let boardData = [];
 
-function addlist(event){
-    event.preventDefault();
-    const ul = document.createElement("ul");
-    const check = document.createElement("input");
-    check.type = 'checkbox';
-    ul.appendChild(check);
-    //ul.innerText = inputField.value;
-    board.appendChild(ul);
-    ul.addEventListener('click', function(){
-        li.style.textDecoration = "line-through";
-    })
-    ul.addEventListener('dblclick', function(){
-        board.removeChild(li);
+// DOM 객체
+const inputForm = document.querySelector('#list form');
+const inputField = document.querySelector('#inputField');
+const board = document.querySelector('#board ul');
 
-    })
+// 로컬스토리지에 저장된 게시글 불러오기
+if(localStorage.getItem('boardData')) {
+  boardData = JSON.parse(localStorage.getItem('boardData'));
+  renderBoard();
 }
 
-window.localStorage
-inputButton.addEventListener("click",addlist);
+// 게시글 추가 함수
+function addBoardItem(event) {
+  event.preventDefault();
+  const inputValue = inputField.value.trim();
+
+  if(inputValue === '') return;
+
+  const newBoardItem = {
+    id: Date.now(),
+    content: inputValue
+  };
+
+  boardData.push(newBoardItem);
+  inputField.value = '';
+  renderBoard();
+  saveData();
+}
+
+// 게시글 삭제 함수
+function deleteBoardItem(event) {
+  if(!event.target.classList.contains('delete')) return;
+
+  const deleteId = parseInt(event.target.parentNode.dataset.id);
+  boardData = boardData.filter(item => item.id !== deleteId);
+  renderBoard();
+  saveData();
+}
+
+// 게시글 렌더링 함수
+function renderBoard() {
+  let html = '';
+  boardData.forEach(item => {
+    html += `
+      <li data-id="${item.id}">
+        <button class="delete">✅</button>
+        <span class="content">${item.content}</span>
+      </li>
+    `;
+  });
+  board.innerHTML = html;
+}
+
+// 데이터 저장 함수
+function saveData() {
+  localStorage.setItem('boardData', JSON.stringify(boardData));
+}
+
+// 이벤트 리스너 등록
+inputForm.addEventListener('submit', addBoardItem);
+board.addEventListener('click', deleteBoardItem);
